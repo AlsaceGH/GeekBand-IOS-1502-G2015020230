a334644d0e95af1b8f356ecb2a1531819f078469
AlsaceGH <alsace.gao@carestream.com>
2015-11-03 22:55:08 -0800

    Merge branch 'master' into Alsace

---
+++ 蓦然/Controllers/CSHLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSHLoginViewController.h
+//  蓦然
+//
+//  Created by alsace on 11/3/15.
+//  Copyright (c) 2015 com.Carestream.heng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSHLoginViewController : UIViewController
+
+@end
