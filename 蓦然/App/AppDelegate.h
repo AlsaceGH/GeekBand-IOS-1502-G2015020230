fefeb186a3d004eca1381cbd4b5e7ff6f6113d16
AlsaceGH <alsace.gao@carestream.com>
2015-11-04 20:03:29 -0800

    Login&Register：Login

---
+++ 蓦然/App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  蓦然
+//
+//  Created by alsace on 11/3/15.
+//  Copyright (c) 2015 com.Carestream.heng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
