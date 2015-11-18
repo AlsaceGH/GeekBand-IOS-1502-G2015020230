fc97df7688a472d50a43d7baf1cfdbe4d9a1ee83
AlsaceGH <alsace@alsaces-Mac.local>
2015-11-18 05:27:57 -0800

    Add latest

---
+++ 蓦然/Controllers/CSHLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  CSHLoginViewController.h
+//  蓦然
+//
+//  Created by alsace on 11/3/15.
+//  Copyright (c) 2015 com.Carestream.heng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSHLoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *emialTextFiled;
+
+
+@end
---
+++ 蓦然/Controllers/CSHLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  CSHLoginViewController.m
+//  蓦然
+//
+//  Created by alsace on 11/3/15.
+//  Copyright (c) 2015 com.Carestream.heng. All rights reserved.
+//
+
+#import "CSHLoginViewController.h"
+
+@interface CSHLoginViewController ()
+
+@end
+
+@implementation CSHLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
